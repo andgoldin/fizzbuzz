3c5a430fd1b392d9e06d9e3c81350cd090697a69
Andrew Goldin <adg2160@columbia.edu>
2014-02-03 05:15:42 -0500

    Create andrew_goldin_adg2160.rb

---
+++ andrew_goldin_adg2160.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(fizz_val, fizz_word, buzz_val, buzz_word, start_val, end_val) 
+    for i in start_val..end_val
+        output = i
+        if i % fizz_val == 0
+           output = fizz_word
+        end
+        if i % buzz_val == 0
+            output = output == fizz_word ? output + buzz_word : buzz_word
+        end
+        puts output
+    end
+end
+
+# call fizzbuzz for multiples of 3 and 5 from 1 to 100 using my name
+fizzbuzz(3, "andrew", 5, "goldin", 100)
