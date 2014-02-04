0aaaa5e3a1b5298acfed77477666089e56ecfc9d
Andrew Goldin <adg2160@columbia.edu>
2014-02-04 17:20:48 -0500

    Update andrew_goldin_adg2160.rb

--- andrew_goldin_adg2160.rb
+++ andrew_goldin_adg2160.rb
@@ -1,14 +1,17 @@
+# performs fizzbuzz over a custom range with custom values/words
 def fizzbuzz(fizz_val, fizz_word, buzz_val, buzz_word, start_val, end_val) 
-    for i in start_val..end_val
-        output = i
+    start_val.upto(end_val) { |i|
         if i % fizz_val == 0
-           output = fizz_word
+           print fizz_word
         end
         if i % buzz_val == 0
-            output = output == fizz_word ? output + buzz_word : buzz_word
+           print buzz_word
         end
-        puts output
-    end
+        if i % fizz_val != 0 && i % buzz_val != 0
+           print i
+        end
+        puts
+    }
 end
 
 # call fizzbuzz for multiples of 3 and 5 from 1 to 100 using my name
